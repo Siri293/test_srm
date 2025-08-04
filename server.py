eabb4181de4a4b296ed4f8b2ad4c824568e9b640
SiriChandana <mandapatisirichandana@gmail.com>
2025-08-04 11:13:45 +0530

    Fix proxy and server port errors

--- server.py
+++ server.py
@@ -64,6 +64,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5050))
+    port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host="0.0.0.0", port=port, debug=True)
 
